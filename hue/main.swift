4dfee9674ea3ea9c152c91c66ccdc4d1268b6c35
Gerrit N. Bouknecht <gbouknecht@server.fake>
2016-08-07 19:26:18 +0200

    Initial commit

---
+++ hue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hue
+//
+//  Created by Gerrit Bouknecht on 07/08/16.
+//  Copyright © 2016 Gerrit Bouknecht. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
